ef2e17c2966836b299d202f8f7e9dd8283705c3d
이쥬곰 <leesu@ijyugom-ui-MacBookAir.local>
2022-08-31 21:32:17 +0900

    feat : footer 기능

--- src/day2/TodoFooter.js
+++ src/day2/TodoFooter.js
@@ -1,13 +1,14 @@
 import React from "react";
+import './TodoFooter.css'
 
 const TodoFooter = () => {
   return (
     <footer>
       <span className="todo-count" data-reactid=".0.2.0">
-        <strong data-reactid=".0.2.0.0">4</strong>
+        <strong data-reactid=".0.2.0.0">d</strong>
         <span data-reactid=".0.2.0.1"></span>
-        <span data-reactid=".0.2.0.2">items</span>
-        <span data-reactid=".0.2.0.3"> left</span>
+        <span data-reactid=".0.2.0.2"></span>
+        <span data-reactid=".0.2.0.3"></span>
       </span>
       <ul className="filters" data-reactid=".0.2.1">
         <li data-reactid=".0.2.1.0"><a href="#/" className="selected" data-reactid=".0.2.1.0.0">All</a></li>
